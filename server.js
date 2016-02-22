982779661c214ab0022f3da69e1fea7b04ed4798
dattadh <hariphaneendra.dattada@lifetech.com>
2016-02-21 17:17:38 -0800

    commit one

--- server.js
+++ server.js
@@ -1,62 +1,35 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 
-var express = require('express');
-var app = express();
-
-var port = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + "/"))
-
-var server = http.createServer(app, function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(port, function() {
-    console.log((new Date()) + 'HTTP Server is listening on port'+port);
 });
 
-// Create Websocket Server
-wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false
+io.on('connection', function(socket){
+
+    socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+   io.emit('chat message',socket.name + ':' + msg);
+  });
+
+   socket.on('join', function(name){
+      socket.name = name;
+      socket.broadcast.emit('chat message',name+' connected');
+      console.log('user connected' +name);
+   }); 
+
+   socket.on('disconnect', function(){
+    console.log('user disconnected');
+    socket.broadcast.emit('chat message', socket.name + ' has disconnected from the chat. ');
+  });
 });
 
-function originIsAllowed(origin) {
-  return true;
-}
 
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' 
-                   + request.origin + ' rejected.');
-      return;
-    }
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-
-    // Case 1: receive message from the client
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF("I am fine!");
-            console.log("I am fine!");
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' 
-                         + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-
-    // Case 2: close the connection
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' 
-                   + connection.remoteAddress + ' disconnected.');
-    });
 });
+
+
