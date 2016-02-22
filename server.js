a8f820c657a476ed1016ca297d2adbf339129dd2
dattadh <hariphaneendra.dattada@lifetech.com>
2016-02-21 17:25:11 -0800

    second commit

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 
@@ -27,7 +30,7 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
+http.listen(port, function(){
   console.log('listening on *:3000');
 
 });
